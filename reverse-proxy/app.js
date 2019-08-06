7941308f956dd7e246bdbc0a6d8ad0ccb4465a6a
Jo Jordens <jo@appsaloon.be>
2019-08-06 16:14:17 +0200

    added sample reverse-proxy server

---
+++ reverse-proxy/app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const compression = require('compression')
+const proxyMiddleWare = require('http-proxy-middleware')
+
+const app = express()
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(compression())
+
+/**
+ * CORS setup
+ */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') return res.sendStatus(200)
+    next()
+})
+
+const sampleAppHost = 'http://sample-app'
+const sampleApiServerHost = 'http://sample-api-server'
+
+/* App server */
+app.use('/app', proxyMiddleWare({
+    target: sampleAppHost,
+    pathRewrite: { '^/app': '/' }
+}))
+
+/* Api server */
+app.use('/api', proxyMiddleWare({
+    target: sampleApiServerHost,
+    pathRewrite: { '^/api': '/' }
+}))
+
+/* If all things fail */
+app.get('/', function (req, res) {
+    res.send('This is my reverse proxy server.')
+})
+
+app.use((err, req, res, next) => {
+    if (err.status === 403) {
+        res.status(403)
+        res.send(err.message)
+    } else {
+        res.status(500)
+        res.json({ error: err })
+    }
+
+    console.error(err)
+})
+
+module.exports = app
---
+++ reverse-proxy/index.js
@@ -0,0 +1,50 @@
+const initLogstash = require('@appsaloon/logger-js').default
+const logstashOptions = {
+    protocol: 'https',
+    hostname: 'elkstack.example.com',
+    path: 'app-backend',
+    site: 'samples/reverse-proxy'
+}
+initLogstash(logstashOptions)
+
+const useGreenlock = process.env.GREENLOCK === 'true'
+
+const app = require('./app.js')
+
+if (useGreenlock) {
+    console.log('using greenlock')
+    require('greenlock-express').create({
+
+        // Let's Encrypt v2 is ACME draft 11
+        version: 'draft-11',
+
+        server: 'https://acme-v02.api.letsencrypt.org/directory',
+        // Note: If at first you don't succeed, stop and switch to staging
+        //server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+
+        // You MUST change this to a valid email address
+        email: 'myemail@example.com',
+
+        // You MUST NOT build clients that accept the ToS without asking the user
+        agreeTos: true,
+
+        // You MUST change these to valid domains
+        // NOTE: all domains will validated and listed on the certificate
+        approvedDomains: ['myapp.example.com'],
+
+        // You MUST have access to write to directory where certs are saved
+        // ex: /home/foouser/acme/etc
+        configDir: '/etc/greenlock/acme/',
+
+        app,
+
+        // Get notified of important updates and help me make greenlock better
+        communityMember: true
+
+        //, debug: true
+
+    }).listen(80, 443)
+} else {
+    console.log('not using greenlock')
+    app.listen(80, () => console.log(`Server listening on port 80`))
+}
