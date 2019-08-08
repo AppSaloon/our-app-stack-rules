4e7fe25067f87a953f973e1a388261975dce3be2
Jo Jordens <jo@appsaloon.be>
2019-08-08 09:43:27 +0200

    added eslint config to reverse proxy

--- reverse-proxy/app.js
+++ reverse-proxy/app.js
@@ -12,13 +12,13 @@ app.use(compression())
  * CORS setup
  */
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
 })
 app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') return res.sendStatus(200)
-    next()
+  if (req.method === 'OPTIONS') return res.sendStatus(200)
+  next()
 })
 /* CORS end */
 
@@ -27,31 +27,31 @@ const sampleApiServerHost = 'http://sample-api-server'
 
 /* App server */
 app.use('/app', proxyMiddleWare({
-    target: sampleAppHost,
-    pathRewrite: { '^/app': '/' }
+  target: sampleAppHost,
+  pathRewrite: { '^/app': '/' }
 }))
 
 /* Api server */
 app.use('/api', proxyMiddleWare({
-    target: sampleApiServerHost,
-    pathRewrite: { '^/api': '/' }
+  target: sampleApiServerHost,
+  pathRewrite: { '^/api': '/' }
 }))
 
 /* If all things fail */
 app.get('/', function (req, res) {
-    res.send('This is my reverse proxy server. :)')
+  res.send('This is my reverse proxy server. :)')
 })
 
 app.use((err, req, res, next) => {
-    if (err.status === 403) {
-        res.status(403)
-        res.send(err.message)
-    } else {
-        res.status(500)
-        res.json({ error: err })
-    }
+  if (err.status === 403) {
+    res.status(403)
+    res.send(err.message)
+  } else {
+    res.status(500)
+    res.json({ error: err })
+  }
 
-    console.error(err)
+  console.error(err)
 })
 
 module.exports = app
